d5021da268051ee5ac90f9d4d876ac636fb34164
Kirk Byers <ktbyers@twb-tech.com>
2019-01-08 12:23:04 -0800

    Class2 exercises

--- class2/exercises/netmiko_ex1b.py
+++ class2/exercises/netmiko_ex1b.py
@@ -1,16 +1,18 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-device2 = {
+device = {
     "device_type": "cisco_ios",
-    "host": "cisco2.lasthop.io",
+    "host": "cisco4.lasthop.io",
     "username": "pyclass",
     "password": getpass(),
 }
 
-net_connect = ConnectHandler(**device2)
+net_connect = ConnectHandler(**device)
 
-output = net_connect.send_command("ping", expect_string=r"Protocol")
+output = net_connect.send_command(
+    "ping", expect_string=r"Protocol", strip_command=False
+)
 output += net_connect.send_command(
     "\n", expect_string=r"Target IP", strip_prompt=False, strip_command=False
 )
---
+++ class2/exercises/netmiko_ex2.py
@@ -0,0 +1,35 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+from datetime import datetime
+
+password = getpass()
+
+nxos2 = {
+    "host": "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_nxos",
+    "global_delay_factor": 2,
+}
+
+net_connect = ConnectHandler(**nxos2)
+
+cmd = "show lldp neighbors detail"
+start_time = datetime.now()
+output = net_connect.send_command(cmd)
+end_time = datetime.now()
+print("#" * 80)
+print(output)
+print("#" * 80)
+print("\n\nExecution Time: {}".format(end_time - start_time))
+print()
+
+cmd = "show lldp neighbors detail"
+start_time = datetime.now()
+output = net_connect.send_command(cmd, delay_factor=8)
+end_time = datetime.now()
+print("#" * 80)
+print(output)
+print("#" * 80)
+print("\n\nExecution Time: {}".format(end_time - start_time))
+print()
