b222da2e03d6c695c558454d4686693dbfa4e635
Kirk Byers <ktbyers@twb-tech.com>
2019-01-09 19:33:41 -0800

    Simplifying the structure for EX3

--- class3/exercises/ex3.py
+++ class3/exercises/ex3.py
@@ -24,14 +24,12 @@ ipv4_list = []
 ipv6_list = []
 
 for intf, ipaddr_dict in nxos_data.items():
-    for k, v in ipaddr_dict.items():
-        if k == 'ipv4':
-            for ip_addr, prefix_dict in v.items():
-                prefix_length = prefix_dict['prefix_length']
+    for ipv4_or_ipv6, addr_info in ipaddr_dict.items():
+        for ip_addr, prefix_dict in addr_info.items():
+            prefix_length = prefix_dict['prefix_length']
+            if ipv4_or_ipv6 == 'ipv4':
                 ipv4_list.append("{}/{}".format(ip_addr, prefix_length))
-        if k == 'ipv6':
-            for ip_addr, prefix_dict in v.items():
-                prefix_length = prefix_dict['prefix_length']
+            elif ipv4_or_ipv6 == 'ipv6':
                 ipv6_list.append("{}/{}".format(ip_addr, prefix_length))
 
 print(ipv4_list)
