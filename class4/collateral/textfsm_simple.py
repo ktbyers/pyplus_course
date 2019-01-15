b81ea9a7ba33295576c3033492947f34d1c9cb2b
Kirk Byers <ktbyers@twb-tech.com>
2019-01-14 17:16:28 -0800

    Updates to improve the simple textfsm integration script

--- class4/collateral/textfsm_simple.py
+++ class4/collateral/textfsm_simple.py
@@ -1,16 +1,19 @@
+from pprint import pprint
 import textfsm
 
-template_file = "show_int_status.template"
+template_file = "show_ip_int_brief.template"
 template = open(template_file)
 
-with open("show_int_status.txt") as f:
+with open("show_ip_int_brief.txt") as f:
     raw_text_data = f.read()
-
-import ipdb
-ipdb.set_trace()
 
 # The argument 'template' is a file handle and 'raw_text_data' is a string.
 re_table = textfsm.TextFSM(template)
 data = re_table.ParseText(raw_text_data)
-
 template.close()
+
+print("\nPrint the header row which could be used for dictionary construction")
+print(re_table.header)
+print("\nOutput Data: ")
+pprint(data)
+print()
