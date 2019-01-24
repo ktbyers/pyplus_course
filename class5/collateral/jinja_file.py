ad04771314fd062cbc897a8e438706a2f3b9359a
Kirk Byers <ktbyers@twb-tech.com>
2019-01-24 13:03:52 -0800

    Class5 exercises

---
+++ class5/collateral/jinja_file.py
@@ -0,0 +1,16 @@
+from jinja2 import Template
+
+filename = "bgp_config.j2"
+with open(filename) as f:
+    my_template = f.read()
+
+template_vars = {
+    "bgp_as": 22, 
+    "router_id": "1.1.1.1", 
+    "peer1": "10.20.30.1",
+}
+
+j2_template = Template(my_template)
+output = j2_template.render(**template_vars)
+print(output)
+
