37698992e3973f2ef29a2f4aca0e64475015c3fe
Kirk Byers <ktbyers@twb-tech.com>
2019-01-24 15:37:35 -0800

    Class5 collateral

---
+++ class5/collateral/INCLUDE/j2_loader.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals, print_function
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+from pprint import pprint
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader(".")
+
+template_vars = {}
+
+template_file = "template1.j2"
+template = env.get_template(template_file)
+output = template.render(**template_vars)
+print(output)
---
+++ class5/collateral/INCLUDE/j2_loader2.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals, print_function
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+from pprint import pprint
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader(".")
+
+template_vars = {
+    "template_var": "second_template.j2",
+}
+
+template_file = "template2.j2"
+template = env.get_template(template_file)
+output = template.render(**template_vars)
+print(output)
