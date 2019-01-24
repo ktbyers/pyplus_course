3562359ca5c9d0087a4fe7f6539ce5812586688e
Kirk Byers <ktbyers@twb-tech.com>
2019-01-24 14:08:27 -0800

    Class5 collateral

--- class5/collateral/jinja_cond3.py
+++ class5/collateral/jinja_cond3.py
@@ -2,14 +2,12 @@ from __future__ import unicode_literals, print_function
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
-# env = Environment()
-# env.loader = FileSystemLoader('.')
 env = Environment(undefined=StrictUndefined)
-env.loader = FileSystemLoader([".", "./templates/"])
+env.loader = FileSystemLoader(".")
 
-my_vars = {"bgp_as": 22, "router_id": "1.1.1.1", "peer1": "10.20.30.1"}
+my_vars = {"mode": ""}
 
-template_file = "bgp_config.j2"
+template_file = "intf_config3.j2"
 template = env.get_template(template_file)
 output = template.render(**my_vars)
 print(output)
--- class5/collateral/jinja_file.py
+++ class5/collateral/jinja_file.py
@@ -4,13 +4,8 @@ filename = "bgp_config.j2"
 with open(filename) as f:
     my_template = f.read()
 
-template_vars = {
-    "bgp_as": 22, 
-    "router_id": "1.1.1.1", 
-    "peer1": "10.20.30.1",
-}
+template_vars = {"bgp_as": 22, "router_id": "1.1.1.1", "peer1": "10.20.30.1"}
 
 j2_template = Template(my_template)
 output = j2_template.render(**template_vars)
 print(output)
-
