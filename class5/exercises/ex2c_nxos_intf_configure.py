7534171f9a661ab68e9610ddbfea1a64c9b843f1
Kirk Byers <ktbyers@twb-tech.com>
2019-01-28 13:50:05 -0800

    Exercise 2c

--- class5/exercises/ex2c_nxos_intf_configure.py
+++ class5/exercises/ex2c_nxos_intf_configure.py
@@ -1,10 +1,11 @@
 import time
+import re
+
 from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
 from netmiko import ConnectHandler
 from my_devices import nxos1, nxos2
-
 
 
 if __name__ == "__main__":
@@ -21,7 +22,7 @@ if __name__ == "__main__":
         "ipv4_address": "10.1.100.1",
         "ipv4_netmask": "24",
     }
-    
+
     nxos2_vars = {
         "device_name": "nxos2",
         "local_as": 22,
@@ -29,7 +30,7 @@ if __name__ == "__main__":
         "ipv4_address": "10.1.100.2",
         "ipv4_netmask": "24",
     }
-    
+
     nxos1_vars["peer_ip"] = nxos2_vars["ipv4_address"]
     nxos2_vars["peer_ip"] = nxos1_vars["ipv4_address"]
 
@@ -50,7 +51,7 @@ if __name__ == "__main__":
         print(cfg)
         # Stripping out whitespace will make CLI cfg-output display cleaner
         cfg_lines = [cfg.strip() for cfg in cfg.splitlines()]
-    
+
         # Establish Netmiko Connection
         net_connect = ConnectHandler(**tmp_device)
         # Store the SSH connection for later
@@ -79,12 +80,12 @@ if __name__ == "__main__":
         output = net_connect.send_command(bgp_verify)
         # Retrieve the State/PfxRcd field which is the last field
         bgp_pattern = r"\s+(\S+)\s*$"
-        match = re.search(r"\s+(\S+)\s*$", out)
+        match = re.search(r"\s+(\S+)\s*$", output)
         prefix_received = match.group(1)
         try:
             # If this is an integer, the BGP session reached the established state
             int(prefix_received)
-            print("BGP reached the established state")
+            print(f"BGP reached the established state. Prefixes received {prefix_recieved}")
         except ValueError:
             print("BGP failed to reach the established state")
         print(output)
