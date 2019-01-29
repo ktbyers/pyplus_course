199124ecb50ad796d4fdeb4758996c0b93fd8161
Kirk Byers <ktbyers@twb-tech.com>
2019-01-28 16:52:20 -0800

    Class5, exercise3

--- class5/exercises/ex3_vrf.py
+++ class5/exercises/ex3_vrf.py
@@ -2,7 +2,7 @@ from jinja2 import FileSystemLoader, StrictUndefined
 from jinja2.environment import Environment
 
 env = Environment(undefined=StrictUndefined)
-env.loader = FileSystemLoader('./templates')
+env.loader = FileSystemLoader('./templates/exercise3/')
 
 j2_vars = {
     'vrf_name': 'blue',
@@ -11,7 +11,9 @@ j2_vars = {
     'ipv6_af': True,
 }
 
+print()
 template_file = "ios_vrf.j2"
 template = env.get_template(template_file)
 cfg = template.render(**j2_vars)
 print(cfg)
+print()
