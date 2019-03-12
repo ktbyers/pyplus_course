0cff0ccf8ae71c2ddeb3f5cc64eb878f343068cf
Kirk Byers <ktbyers@twb-tech.com>
2019-03-11 21:01:20 -0700

    Class7 exercises

--- class7/exercises/lesson1.py
+++ class7/exercises/lesson1.py
@@ -1,85 +1,59 @@
 # Python2/3 Compatibility
 from __future__ import unicode_literals, print_function
-# Import etree to handle our xml file
 from lxml import etree
 
 
-# Using a context manager; open file in 'read' mode
+# Using a context manager read in the XML file
 with open("show_security_zones.xml", "r") as infile:
-    # Use etree.fromstring to read in the file
+    # Parse string using etree.fromstring
     show_security_zones = etree.fromstring(infile.read())
 
-# Printing out the newly created variable
-print()
-print("Print the new variable and type of the new variable")
+# Exercise 1a
+print("\n\n")
+print("Print the XML variable and its type.")
 print("-" * 20)
 print(show_security_zones)
 print(type(show_security_zones))
 
-# Print out the entirety of the XML file -- make sure to set "pretty_print" to
-# True. Is the output "pretty"? Why not? Print it again, but ensuring that the
-# otuput is "pretty".
-print()
-print("Print the XML object")
+# Exercise 1b
+print("\n\n")
+print("Print the XML tree")
 print("-" * 20)
-print(etree.tostring(show_security_zones, pretty_print=True))
+print(etree.tostring(show_security_zones).decode())
+# Alternate solution
+# print(etree.tostring(show_security_zones, encoding="unicode"))
 
-# Two methods to decode to "pretty" the output:
-print()
-print("Print the 'pretty' XML object method 1")
-print("-" * 20)
-print(etree.tostring(show_security_zones, pretty_print=True).decode())
-print()
-print("Print the 'pretty' XML object method 2")
-print("-" * 20)
-print(etree.tostring(show_security_zones, pretty_print=True,
-                     encoding="unicode"))
-
-# Before attempting to access child elements, print out the value of the tag
-# at the root of the tree -- this should be the "security" tag
-print()
-print("Print the top level name tag at the root of XML object")
+# Exercise 1c
+print("\n\n")
+print("Print the name (tag) of the root element")
 print("-" * 20)
 print(show_security_zones.tag)
-
-# Determine the quantity of child elements of the "security" tag; this will
-# help us understand the trees structure so that we can properly traverse it
 print()
-print("Print the length of the root XML object")
+print("Print the number of direct children of the root")
 print("-" * 20)
 print(len(show_security_zones))
 
-# Access the child element via list indicies; print the tag method 1
-print()
-print("Print the child element and child element name tag method 1")
+# Exercise 1d
+print("\n\n")
+print("Print the child tag using list indices")
 print("-" * 20)
-print(show_security_zones[0])
 print(show_security_zones[0].tag)
 
-# Access the child element via getchildren; print the tag method 2
 print()
-print("Print the child element and child element name tag method 2")
+print("Print the child tag using getchildren()")
 print("-" * 20)
-print(show_security_zones.getchildren()[0])
 print(show_security_zones.getchildren()[0].tag)
 
-# Create 'trust_zone' variable; access text of 'name' child tag
-print()
-print("Create 'trust_zone' variable and print text of the name child tag")
+# Exercise 1e
+print("\n\n")
+print("Create 'trust_zone' variable and print text of the 'name' child tag")
 print("-" * 20)
 trust_zone = show_security_zones[0][0]
-print(trust_zone[0].text)
+print(f"Security zone: {trust_zone[0].text}")
 
-# Iterate through all children of 'trust_zone' print tags of children
-print()
-print("Iterate through all children of 'trust_zone', print tags of children")
+# Exercise 1f
+print("\n\n")
+print("All children of the 'trust_zone'")
 print("-" * 20)
 for child in trust_zone:
     print(child.tag)
-
-# Print the tag "security" as that is the root of all of the elements we've
-# been working with in this exercise
-print()
-print("Print the tag for root of the previous child element")
-print("-" * 20)
-print(child.getroottree().getroot().tag)
