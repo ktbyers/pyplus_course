b4dd6cc3489c8bcabce5d29f2a1678d0f9b3c43b
Kirk Byers <ktbyers@twb-tech.com>
2019-03-13 14:50:09 -0700

    Class7 exercises

--- class7/exercises/lesson2.py
+++ class7/exercises/lesson2.py
@@ -3,8 +3,7 @@ from pprint import pprint
 import xmltodict
 
 
-# Using a context manager; open file in 'read' mode
-with open("show_security_zones.xml", "r") as infile:
+with open("show_security_zones.xml") as infile:
     show_security_zones = xmltodict.parse(infile.read())
 
 
@@ -14,9 +13,8 @@ print("-" * 20)
 pprint(show_security_zones)
 print(type(show_security_zones))
 
-# Print out the number and name of all the security zones
 print("\n\n")
-print("Print out index and name of security zones")
+print("Print out index and name of the security zones")
 print("-" * 20)
 zones = show_security_zones["zones-information"]["zones-security"]
 for index, zone in enumerate(zones):
