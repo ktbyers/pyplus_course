5707c0de95acc09bfb88a2a94eebcd52cbe57c50
Kirk Byers <ktbyers@twb-tech.com>
2019-03-13 15:33:53 -0700

    Class7 exercises

--- class7/exercises/lesson3.py
+++ class7/exercises/lesson3.py
@@ -1,10 +1,16 @@
 from __future__ import unicode_literals, print_function
 import xmltodict
 
-
-def read_xml(filename, force_list=False):
+def read_xml_forcelist(filename, force_list=None):
+    if force_list is None:
+        force_list = {}
     with open(filename, "r") as infile:
         return xmltodict.parse(infile.read(), force_list=force_list)
+
+
+def read_xml(filename):
+    with open(filename, "r") as infile:
+        return xmltodict.parse(infile.read())
 
 
 if __name__ == "__main__":
@@ -15,41 +21,20 @@ if __name__ == "__main__":
     filename = "show_security_zones_trust.xml"
     show_security_zones_single = read_xml(filename)
 
-    # Print out the type of the element 'security-zone' in original XML document
-    print("Type of 'security-zone' in original XML file")
+    print("\n\n")
+    print("Type of 'zones-security' in the first XML file:")
     print("-" * 20)
-    print(type(show_security_zones['zones-information']['zones-security']))
+    print(type(show_security_zones["zones-information"]["zones-security"]))
 
-    # Print out the type of the element 'security-zone' in updated XML document
-    print()
-    print("Type of 'security-zone' in modified XML file with only one zone")
+    print("\n\n")
+    print("Type of 'zones-security' in the single-zone XML file:")
     print("-" * 20)
-    print(type(show_security_zones_single['zones-information']['zones-security']))
+    print(type(show_security_zones_single["zones-information"]["zones-security"]))
 
+    print("\n\n")
+    print("Type of 'zones-security' in the single-zone XML file using force_list:")
+    print("-" * 20)
     filename = "show_security_zones_trust.xml"
-    show_security_zones_single = read_xml(filename, force_list=True)
-    print(type(show_security_zones_single['zones-information']['zones-security']))
-
-
-## Using a context manager; open file in 'read' mode; force single elements to lists
-#with open("show_security_zones.xml", "r") as infile:
-#    # Use etree.fromstring to read in the file
-#    show_security_zones = xmltodict.parse(infile.read(), force_list=True)
-#
-#
-## Using a context manager; open file in 'read' mode; force single elements to lists
-#with open("show_security_zones_single_zone.xml", "r") as infile:
-#    # Use etree.fromstring to read in the file
-#    show_security_zones_single = xmltodict.parse(infile.read(), force_list=True)
-#
-## Print out the type of the element 'security-zone' in original XML document
-#print()
-#print("Type of 'security-zone' in original XML file")
-#print("-" * 20)
-#print(type(show_security_zones["security"][0]["zones"][0]["security-zone"]))
-#
-## Print out the type of the element 'security-zone' in updated XML document
-#print()
-#print("Type of 'security-zone' in modified XML file with only one zone")
-#print("-" * 20)
-#print(type(show_security_zones_single["security"][0]["zones"][0]["security-zone"]))
+    show_security_zones_single = read_xml_forcelist(filename, force_list={"zones-security": True})
+    print(type(show_security_zones_single["zones-information"]["zones-security"]))
+    print("\n\n")
