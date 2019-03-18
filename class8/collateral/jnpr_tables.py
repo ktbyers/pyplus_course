13de4933d0549235182e4b5f40b002f4653c6c93
Kirk Byers <ktbyers@twb-tech.com>
2019-03-18 14:41:04 -0700

    PyEZ exercises

---
+++ class8/collateral/jnpr_tables.py
@@ -0,0 +1,10 @@
+from jnpr.junos import Device
+from jnpr.junos.op.arp import ArpTable
+from getpass import getpass
+from pprint import pprint
+
+a_device = Device(host='srx2.lasthop.io', user='pyclass', password=getpass())
+a_device.open()
+
+arp_entries = ArpTable(a_device)
+arp_entries.get()
