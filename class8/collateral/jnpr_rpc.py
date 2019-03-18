2802a55124285e1180a6f01d04b902f24bffeaee
Kirk Byers <ktbyers@twb-tech.com>
2019-03-18 14:52:53 -0700

    Jnpr direct RPC

---
+++ class8/collateral/jnpr_rpc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function, unicode_literals
+
+from jnpr.junos import Device
+from lxml import etree
+from getpass import getpass
+
+juniper_srx = {"host": "srx2.lasthop.io", "user": "pyclass", "password": getpass()}
+
+a_device = Device(**juniper_srx)
+a_device.open()
+
+# show version | display xml rpc
+# <get-software-information>
+# xml_out = a_device.rpc.get_software_information()
+# print(etree.tostring(xml_out, encoding="unicode"))
+
+# get_lldp_neighbors_information()
+#xml_out = a_device.rpc.get_lldp_neighbors_information()
+xml_out = a_device.rpc.get_zones_information()
+print(etree.tostring(xml_out, encoding="unicode"))
