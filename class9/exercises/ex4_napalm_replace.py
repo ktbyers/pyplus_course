e655480225e18dbb62a17ab56582ee7a8b5731a5
Kirk Byers <ktbyers@twb-tech.com>
2019-03-21 14:46:14 -0700

    Class9 exercises

--- class9/exercises/ex4_napalm_replace.py
+++ class9/exercises/ex4_napalm_replace.py
@@ -1,6 +1,12 @@
 from my_functions import open_napalm_connection, create_checkpoint
 from my_devices import nxos1
 
+# Disable Self-signed Certificate Warnings
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 NXOS_REPLACE_CANDIDATE = "nxos1_replacement_cfg"
 
@@ -12,17 +18,17 @@ if __name__ == "__main__":
     create_checkpoint(conn)
 
     print("\n\n")
-    print("Diff before and after loading replace candidate: nxos only")
-    print("-" * 20)
     conn.load_replace_candidate(NXOS_REPLACE_CANDIDATE)
-    print("Diff before commiting for device {}".format(conn.hostname))
+    print("Config staged: pending differences {}".format(conn.hostname))
     print(">" * 8)
     print(conn.compare_config())
     print(">" * 8)
 
-    print("\nDiscarding candidate config for device {}".format(conn.hostname))
+    print("\n\n")
+    print("Discarding candidate config for device {}".format(conn.hostname))
     conn.discard_config()
     print("Diff after discarding candidate config for device {}".format(conn.hostname))
     print(">" * 8)
     print(conn.compare_config())
     print(">" * 8)
+    print("\n\n")
