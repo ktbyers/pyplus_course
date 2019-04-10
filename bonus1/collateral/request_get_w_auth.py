abb94ddad4d4d4a26af334019477b76279375fbd
Kirk Byers <ktbyers@twb-tech.com>
2019-04-10 11:17:13 -0700

    REST API collateral

---
+++ bonus1/collateral/request_get_w_auth.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from pprint import pprint
+
+
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+if __name__ == "__main__":
+
+    token = os.environ["NETBOX_TOKEN"]
+    # url = "https://netbox.lasthop.io/api/dcim/devices"
+    url = "https://netbox.lasthop.io/api/dcim/devices/1"
+    http_headers = {"accept": "application/json; version=2.4;"}
+    if token:
+        http_headers["authorization"] = "Token {}".format(token)
+
+    response = requests.get(url, headers=http_headers, verify=False)
+    response = response.json()
+
+    print()
+    pprint(response)
+    print()
