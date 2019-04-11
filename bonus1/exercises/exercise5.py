3e0da21bcad70f22021ba7082eb653fa99e83a20
Kirk Byers <ktbyers@twb-tech.com>
2019-04-10 21:26:17 -0700

    Exercises

---
+++ bonus1/exercises/exercise5.py
@@ -0,0 +1,45 @@
+import os
+import json
+import requests
+from pprint import pprint
+
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+# Constants, including ID from previously created object
+BASE_URL = "https://netbox.lasthop.io/api/"
+ADDRESS_ID = "48"
+
+
+def main():
+
+    token = os.environ["NETBOX_TOKEN"]
+
+    # HTTP PUT needs the "Content-Type" header instead of "accept"
+    http_headers = {}
+    http_headers["Content-Type"] = "application/json; version=2.4;"
+    http_headers["accept"] = "application/json; version=2.4;"
+    http_headers["Authorization"] = f"Token {token}"
+
+    # Add a description field to our IP object
+    data = {"address": "1.1.1.1/32", "description": "Rest-API testing"}
+
+    # PUT (see notes in lesson about PATCH) to update our IP with a description
+    resp = requests.put(
+        f"{BASE_URL}ipam/ip-addresses/{ADDRESS_ID}/",
+        headers=http_headers,
+        verify=False,
+        data=json.dumps(data),
+    )
+    print()
+    print(f"Response code: {resp.status_code}")
+    print("Returned JSON:")
+    print("-" * 12)
+    pprint(resp.json())
+    print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ bonus1/exercises/exercise6.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from pprint import pprint
+
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+# Constants
+BASE_URL = "https://netbox.lasthop.io/api/"
+MY_IP_ID = "50"
+
+
+def main():
+
+    token = os.environ["NETBOX_TOKEN"]
+
+    # HTTP delete needs the "Content-Type" header instead of "accept"
+    http_headers = {}
+    http_headers["Content-Type"] = "application/json; version=2.4;"
+    http_headers["accept"] = "application/json; version=2.4;"
+    http_headers["Authorization"] = f"Token {token}"
+
+    # Delete our previously created IP address
+    print()
+    print("Deleting IP address object.")
+    resp = requests.delete(
+        f"{BASE_URL}ipam/ip-addresses/{MY_IP_ID}/", headers=http_headers, verify=False
+    )
+    print(f"Response code: {resp.status_code}")
+    print()
+
+
+if __name__ == "__main__":
+    main()
