c546e61d2454806b59dc9cd9a83bc2f0f2f0f5b3
Kirk Byers <ktbyers@twb-tech.com>
2019-04-17 11:32:30 -0700

    Bonus2 exercises

---
+++ bonus2/exercises/fixme_black.py
@@ -0,0 +1,40 @@
+from my_devices import network_devices
+from napalm import get_network_driver
+from pprint import pprint
+
+
+def open_napalm_connection(device):
+    """Funtion to open napalm connection and return connection object"""
+    # Copy dictionary to ensure original object is not modified
+    device = device.copy()
+    # Pop "platform" as this is an invalid kwarg to napalm
+    platform = device.pop("platform")
+    driver = get_network_driver(platform)
+    conn = driver(**device)
+    conn.open()
+    return conn
+
+
+def main():
+    connections = []
+    for device in network_devices:
+        conn = open_napalm_connection(device)
+        connections.append(conn)
+
+    print("\n\n")
+    print("Print facts for all devices in connections list")
+    print("-" * 20)
+    for conn in connections:
+        print()
+        print("-" * 6)
+        print(conn)
+        pprint("{} facts:".format(conn.platform))
+        pprint(conn.get_facts())
+        print("-" * 6)
+        # Close the NAPALM connection
+        conn.close()
+    print("\n\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ bonus2/exercises/my_devices.py
@@ -0,0 +1,30 @@
+from getpass import getpass
+
+
+username = "pyclass"
+password = getpass()
+
+cisco3 = {
+    "hostname": "cisco3.lasthop.io",
+    "username": username,
+    "password": password,
+    "platform": "ios",
+}
+
+arista1 = {
+    "hostname": "arista1.lasthop.io",
+    "username": username,
+    "password": password,
+    "platform": "eos",
+}
+
+nxos1 = {
+    "hostname": "nxos1.lasthop.io",
+    "username": username,
+    "password": password,
+    "platform": "nxos",
+    "optional_args": {"port": 8443},
+}
+
+# List of devices (only cisco3 and arista1)
+network_devices = [cisco3, arista1]
