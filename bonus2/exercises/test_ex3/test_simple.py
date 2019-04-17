ed4a861c21a3c16ebede5e0ee70c6abc0a5e3189
Kirk Byers <ktbyers@twb-tech.com>
2019-04-17 13:40:24 -0700

    Bonus2 exercises

---
+++ bonus2/exercises/test_ex3/test_simple.py
@@ -0,0 +1,20 @@
+def my_add(x, y):
+    """Simple function to add two integers together"""
+    return x + y
+
+
+def my_mul(x, y):
+    """Simple function to multiply two integers together"""
+    return x * y
+
+
+def test_my_add():
+    """Simple test to test the "my_ad" function"""
+    assert my_add(5, 11) == 16
+    assert my_add(1, 9) == 1
+
+
+def test_my_mul():
+    """Simple test to test the "my_mul" function"""
+    assert my_mul(2, 7) == 14
+    assert my_mul(9, 9) == 81
