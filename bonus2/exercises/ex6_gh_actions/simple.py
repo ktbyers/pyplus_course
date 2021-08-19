b8cdeb61ea2abd5607e5da41134bedd116144220
Kirk Byers <ktbyers@twb-tech.com>
2021-08-18 20:01:49 -0700

    Adding new GH actions exercise6

---
+++ bonus2/exercises/ex6_gh_actions/simple.py
@@ -0,0 +1,8 @@
+def my_sum(x, y):
+    return x + y
+
+
+if __name__ == "__main__":
+
+    for _ in range(10):
+        print("Hello world")
---
+++ bonus2/exercises/ex6_gh_actions/test_simple.py
@@ -0,0 +1,7 @@
+from simple import my_sum
+
+
+def test_my_sum():
+    assert my_sum(5, 6) == 11
+    assert my_sum(1, 2) == 3
+    assert my_sum(3, 4) == 7
