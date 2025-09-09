9a6e64d61b8c36f9ea7efece65928e862eb99925
Kirk Byers <ktbyers@twb-tech.com>
2025-09-09 10:40:36 -0700

    Updating/changing class2, exercise2

--- class2/exercises/netmiko_ex2.py
+++ class2/exercises/netmiko_ex2.py
@@ -1,37 +1,23 @@
 import os
 from netmiko import ConnectHandler
 from getpass import getpass
-from datetime import datetime
 
 password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
-
 nxos2 = {
     "host": "nxos2.lasthop.io",
     "username": "pyclass",
     "password": password,
     "device_type": "cisco_nxos",
-    "global_delay_factor": 2,
 }
 
 net_connect = ConnectHandler(**nxos2)
 
 cmd = "show lldp neighbors detail"
-start_time = datetime.now()
 output = net_connect.send_command(cmd)
-end_time = datetime.now()
+print()
 print("#" * 80)
 print(output)
 print("#" * 80)
-print("\n\nExecution Time: {}".format(end_time - start_time))
 print()
 
-cmd = "show lldp neighbors detail"
-start_time = datetime.now()
-output = net_connect.send_command(cmd, delay_factor=8)
 net_connect.disconnect()
-end_time = datetime.now()
-print("#" * 80)
-print(output)
-print("#" * 80)
-print("\n\nExecution Time: {}".format(end_time - start_time))
-print()
